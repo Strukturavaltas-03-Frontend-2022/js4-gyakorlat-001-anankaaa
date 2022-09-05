a75e9b14566c3d3e459f362894136f0a6d8117e4
Anankaaa <hegyaljai83@hotmail.com>
2022-09-05 20:53:36 +0200

    done

--- 13-promise/exercises/01/solution/js/convertToUppercase.js
+++ 13-promise/exercises/01/solution/js/convertToUppercase.js
@@ -1,1 +1,12 @@
+const convertToUppercase = (array) => {
+  return new Promise((resolve, reject) => {
+    if (array.every(element => typeof element === 'string')) {
+      let newArray = array.map(element => element.toUpperCase());
+      resolve(newArray);
+    } else {
+      reject(new Error('Error: Not all items in the array are strings!'));
+    }
+  })
+}
+
 export default convertToUppercase;
